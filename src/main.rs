3f39ae8b67c9592d6ffb8b09f875ce5a0280a03a
he00741098 <86698067+he00741098@users.noreply.github.com>
2023-04-01 01:21:07 +0000

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use reqwest::header::AUTHORIZATION;
 use std::env;
 use std::collections::HashMap;
+use serde_json::Value;
 #[tokio::main]
 async fn main() {
 		let post_endpoint = "https://api.assemblyai.com/v2/transcript";
@@ -14,43 +15,46 @@ async fn main() {
 		
 	let client = reqwest::Client::new();
 	let res = client.post(post_endpoint)
-		.header(AUTHORIZATION, api_token)
+		.header(AUTHORIZATION, &api_token)
 		.json(&params)
 		.send()
 		.await;
 	let res = res.expect("response not found");
-	println!("{:?}", res.status());
-	println!("{:?}", res.headers());
-	let input = res.text().await.expect("no text")
-	println!("{}", input);
-	let root: Value = serde_json::from_str(input).expect("unable to parse response");
+	//println!("{:?}", res.status());
+	//println!("{:?}", res.headers());
+	let input = res.text().await.expect("no text");
+	//println!("{}", input);
+	let root: Value = serde_json::from_str(&input).expect("unable to parse response");
 
 	// access element using .get()
 	let id = root.get("id").expect("id not found").as_str();
 
-	println!("id = {}", id); // = Some("a hostname")
+	println!("id = {:?}", id); // = Some("a hostname")
 	
 
 
 	//send get request
-	let get_endpoint = post_endpoint+id;
-	let mut result = "";
+	let get_endpoint = post_endpoint.to_owned()+"/"+id.expect("id");
+	println!("get endpoint: {}", get_endpoint);
+	//let result = "";
 	loop{
-	let res = client.get(get_endpoint)
-		.header(AUTHORIZATION, api_token)
+	let res = client.get(&get_endpoint)
+		.header(AUTHORIZATION, &api_token)
 		.send()
 		.await;
 	let text = res.expect("response not found").text().await.expect("unable to parse response");
-	let root: Value = serde_json::from_str(text).expect("unable to parse response");
+	let root: Value = serde_json::from_str(&text).expect("unable to parse response");
 
 	// access element using .get()
-	let id = root.get("id").expect("id not found").as_str();
-	let status = root.get("status").expect("status not found").as_str();
+	//let id = root.get("id").expect("id not found").as_str();
+	let status = root.get("status").expect("status not found").as_str().expect("status not found");
 	if status == "error"{
+		println!("{}", text);
+		panic!("error");
 		break;
 	}
 	if status == "completed"{
-		let result = root.get("text").expect("status not found").as_str();
+		let result = root.get("text").expect("status not found").as_str().expect("status not found");
 		println!("{}",result);
 		break;
 	}
